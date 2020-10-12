094e0b221779ed8ca3236ece4490be922a58f831
Ronald Russo <RonaldRusso94@yahoo.com>
2020-10-12 00:46:15 -0400

    db config

--- config/database.js
+++ config/database.js
@@ -1,19 +1,15 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.v9d7h.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'coffee-trailer-backend'),
-        username: env('DATABASE_USERNAME', 'test'),
-        password: env('DATABASE_PASSWORD', 'Abcd1234'),
+        client: "mongo",
+        uri: env("DATABASE_URI"),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
   },
