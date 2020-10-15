474bfaadab2e2267e422e7454a05ea1b2ed50ba3
TNORTHERN <traynorthern@yahoo.com>
2020-10-15 18:38:52 -0500

    Implement graphql

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: "https://coffee-trailer.herokuapp.com",
+  // url: "https://coffee-trailer.herokuapp.com",
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'b3ab738876c91c33be341d87f7977360'),
