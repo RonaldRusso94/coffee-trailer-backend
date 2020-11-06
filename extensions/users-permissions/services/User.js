2ae0066e5f2979253cf4aa797084cb42f6d915ca
Tyler_Zoltowski <zoltowskit@gmail.com>
2020-11-06 15:14:47 -0500

    Switching to Recipes

--- extensions/users-permissions/services/User.js
+++ extensions/users-permissions/services/User.js
@@ -7,10 +7,7 @@
 
 module.exports = {
     async fetchAuthenticatedUser(id) {
-        const user =  await strapi.query("user", "users-permissions").findOne({ id }, ["role", "orders"]);
-        const favorites = await strapi.query('favorite').find({user: id,  _sort: 'count:desc', _limit: 10}, ["menu_item"]);
-        const favoriteMenuItem = favorites.map(favorite => favorite.menu_item )
-        user.favorites = favoriteMenuItem
+        const user =  await strapi.query("user", "users-permissions").findOne({ id }, ["role", "orders", "recipes"]);
         return user;
     }
 };
