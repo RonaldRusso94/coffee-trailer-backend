c4af4f018394ee83c18982f993e551d233fa45f1
tnorthern <traynorthern@yahoo.com>
2021-01-20 11:44:22 -0600

    Add price to orders type

--- api/orders/config/schema.graphql.js
+++ api/orders/config/schema.graphql.js
@@ -5,15 +5,30 @@ module.exports = {
     user: UsersPermissionsUser
     promos: [Promos]
     order_items: [OrderItem]
-    }`,
+    }
+    extend type Orders {
+      price: Int
+    }
+    `,
   query: `
       orderWithPrice (id: ID!): OrderPrice
     `,
   resolver: {
     Query: {
       orderWithPrice: {
-        description: 'Return the restaurants open by the category',
-        resolver: 'application::orders.orders.findOne', // Will apply the same policy on the custom resolver as the controller's action `findByCategories`.
+        description: "Return the restaurants open by the category",
+        resolver: "application::orders.orders.findOne", // Will apply the same policy on the custom resolver as the controller's action `findByCategories`.
+      },
+    },
+    Orders: {
+      price: {
+        description: "Current price of order",
+        resolverOf: "application::orders.orders.findOne", // Will apply the same policy on the custom resolver as the controller's action `findByCategories`.
+        resolver: (obj) => {
+          const price = strapi.services.orders.findPrice(obj);
+
+          return price;
+        },
       },
     },
   },
